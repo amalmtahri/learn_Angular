06957b78dd1f4eaf62c56b9a884f7b3e6970472f
ext_mtahri <mtahriamal0@gmail.com>
2021-12-28 15:05:15 +0100

    same change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'events-list',
+    templateUrl:'events.component.html',
+
+})
+export class EventsListComponent{
+
+}
