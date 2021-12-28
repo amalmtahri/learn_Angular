ccbd5f1e7ea2b897f1574b9835050aed6734d583
ext_mtahri <mtahriamal0@gmail.com>
2021-12-28 15:28:19 +0100

    update

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,5 +6,18 @@ import { Component } from "@angular/core";
 
 })
 export class EventsListComponent{
+    event = {
+        id:1,
+        name:'Angular connect',
+        date:'16/10/2000',
+        time:"10:10 am",
+        price: 500.00,
+        imageUrl : '../../assets/images/',
+        location: {
+            address : '1028 DT',
+            city: 'London',
+            country : 'England'
+        }
 
+    }
 }
